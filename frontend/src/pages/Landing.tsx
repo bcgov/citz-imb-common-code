56c54ed7e4bf4c54cfb1db2d9c395efba122e269
BradyMitch <braden.jr.mitch@gmail.com>
2024-03-08 15:20:05 -0800

    Added design-system header, adjusted styles, fixed scripts

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { PageLayout } from 'components/common';
 
 const Landing = () => {
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <PageLayout>
+      <h1>Hello World</h1>
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a magna vitae neque semper
+        lacinia quis sed lacus. Donec suscipit finibus justo at scelerisque. Nam eu diam gravida,
+        vehicula nibh et, sagittis quam. Phasellus tincidunt cursus magna quis semper. Quisque et
+        porttitor orci, ut dictum risus. Nam mattis egestas pretium. Phasellus et erat id nulla
+        cursus accumsan.
+      </p>
+    </PageLayout>
   );
 };
 
