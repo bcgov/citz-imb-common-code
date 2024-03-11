89fa2bde8b9992200be1819617747883a86ee1b4
BradyMitch <braden.jr.mitch@gmail.com>
2024-03-11 15:11:14 -0700

    Added stack component

---
+++ frontend/src/components/common/Stack.tsx
@@ -0,0 +1,39 @@
+import './styles.css';
+import React, { CSSProperties, ReactNode, memo, useMemo } from 'react';
+
+type StackProps = {
+  children: ReactNode;
+  direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+  gap?: string;
+  alignItems?: 'center' | 'flex-start' | 'flex-end' | 'stretch';
+  flexWrap?: 'wrap' | 'nowrap' | 'wrap-reverse';
+  additionalStyles?: CSSProperties;
+};
+
+const useStyles = (props: StackProps) => {
+  const {
+    direction = 'row',
+    gap = '5px',
+    alignItems,
+    flexWrap = 'nowrap',
+    additionalStyles,
+  } = props;
+
+  // Memoize for better performance.
+  return useMemo(() => {
+    const styles: CSSProperties = { display: 'flex', flexDirection: direction, gap, flexWrap };
+
+    // Align items.
+    if (alignItems) styles.alignItems = alignItems;
+
+    return { ...styles, ...additionalStyles };
+  }, [direction, gap, flexWrap, alignItems, additionalStyles]);
+};
+
+const StackComponent = (props: StackProps) => {
+  const styles = useStyles(props);
+
+  return <div style={styles}>{props.children}</div>;
+};
+
+export const Stack = memo(StackComponent);
