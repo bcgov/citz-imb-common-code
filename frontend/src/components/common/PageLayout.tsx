2488cd44970a375864e46592fdae805c2dfc9d79
BradyMitch <braden.jr.mitch@gmail.com>
2024-03-26 15:45:27 -0700

    Made PageLayout margin responsive

--- frontend/src/components/common/PageLayout.tsx
+++ frontend/src/components/common/PageLayout.tsx
@@ -5,7 +5,7 @@ type PageLayoutProps = {
 };
 
 const styles = {
-  margin: '20px 210px', // Top/Bottom: 20px, Left/Right: 210px
+  margin: '20px 14%', // Top/Bottom: 20px, Left/Right: 14% (responsive)
   minHeight: 'calc(100dvh - 105px)', // Header: 65px + Margin: 40px = 105px offset
 };
 
