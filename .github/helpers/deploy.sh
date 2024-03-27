a186951b5c1ae364232b675f493eb1d0adf53fb2
Adam Kroon <Adam.Kroon@gov.bc.ca>
2024-03-27 10:30:38 -0700

    reverting deploy.sh changes

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -5,6 +5,5 @@
 oc process -f /home/runner/work/citz-imb-common-code/citz-imb-common-code/openshift/templates/"$DC_TEMPLATE" --namespace=$NAMESPACE \
     -p LICENSE_PLATE="$LICENSE_PLATE" \
     -p IMAGE_TAG=$IMAGE_TAG \
-    -p ENVIRONMENT=$ENVIRONMENT \
-    -p ARTIFACT_PULL_SECRET=$ARTIFACT_PULL_SECRET | \
+    -p ENVIRONMENT=$ENVIRONMENT | \
     oc apply -f -
