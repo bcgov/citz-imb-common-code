4d5e16a86fd1e575ebdc08017a297f75684bd122
Adam Kroon <Adam.Kroon@gov.bc.ca>
2024-03-27 09:04:37 -0700

    MVP-80: Forgot to remove pipe

--- .github/helpers/deploy.sh
+++ .github/helpers/deploy.sh
@@ -5,6 +5,6 @@
 oc process -f /home/runner/work/citz-imb-common-code/citz-imb-common-code/openshift/templates/"$DC_TEMPLATE" --namespace=$NAMESPACE \
     -p LICENSE_PLATE="$LICENSE_PLATE" \
     -p IMAGE_TAG=$IMAGE_TAG \
-    -p ENVIRONMENT=$ENVIRONMENT | \
+    -p ENVIRONMENT=$ENVIRONMENT \
     -p ARTIFACT_PULL_SECRET=$ARTIFACT_PULL_SECRET | \
     oc apply -f -
