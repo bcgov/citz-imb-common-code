8bdeb9402a48ecd73fd9be56b5514b9782456b85
BradyMitch <braden.jr.mitch@gmail.com>
2024-04-02 16:20:22 -0700

    Made Greeting its own component

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,35 +1,11 @@
-import React from 'react';
-import { useSSO } from '@bcgov/citz-imb-sso-react';
-import { PageLayout, Stack, Typography } from 'components/common';
+import { PageLayout, Stack } from 'components/common';
 import { PackageCard } from 'components/cards';
+import { Greeting } from 'components/text';
 
 const Landing = () => {
-  const { user, isAuthenticated } = useSSO();
-
-  // Function to generate greeting based on time of day
-  const getTimeBasedGreeting = (): string => {
-    const hour = new Date().getHours();
-    if (hour < 12) return 'Good morning'; // From 0:00 (midnight) to 11:59
-    if (hour < 18) return 'Good afternoon'; // From 12:00 (noon) to 17:59 [5:59pm]
-    return 'Good evening'; // From 18:00 [6:00pm] to 23:59 [11:59pm]
-  };
-
   return (
     <PageLayout>
-      {isAuthenticated && (
-        <>
-          <Typography display="flex" size="large" margin="20px 0 5px 0">
-            {getTimeBasedGreeting()}&nbsp;
-            <Typography color="blue" bold>
-              {user?.first_name}
-            </Typography>
-            !
-          </Typography>
-        </>
-      )}
-      <Typography size="large" margin="0 0 25px 0">
-        Browse our code offerings below and click on one for more details.
-      </Typography>
+      <Greeting />
       <Stack gap="20px">
         <PackageCard
           icon="Authenticate"
