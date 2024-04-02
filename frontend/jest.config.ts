6bbc62a2903c26529646f9d8c5e8ce276aa06614
Scott Toews <scott.toews@gov.bc.ca>
2024-04-02 10:45:37 -0700

    configure jest

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'jest';
 
 const config: Config = {
   verbose: true,
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src', '<rootDir>/__tests__'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
