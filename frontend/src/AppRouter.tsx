9e3f9557842a42532e681975d480c234133ac322
BradyMitch <braden.jr.mitch@gmail.com>
2024-04-03 10:24:53 -0700

    Added title and links to package page

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -54,6 +54,7 @@ const AppRouter = () => {
               <Suspense fallback={<Spinner />}>
                 <Pages.Package
                   repo="citz-imb-sso-react"
+                  title="SSO React"
                   summary="This npm package offers an integration solution for React applications requiring authentication through the B.C. government's Single Sign-On SSO (CSS) service. It abstracts the complexity of handling SSO protocols manually. By using this package, developers can quickly implement authentication and authorization in their React applications to meet B.C. government security standards."
                 />
               </Suspense>
@@ -66,6 +67,7 @@ const AppRouter = () => {
               <Suspense fallback={<Spinner />}>
                 <Pages.Package
                   repo="citz-imb-sso-express"
+                  title="SSO Express"
                   summary="This npm package offers an integration solution for Express applications requiring authentication through the B.C. government's Single Sign-On SSO (CSS) service. It abstracts the complexity of handling SSO protocols manually. By using this package, developers can quickly implement authentication and authorization in their Express applications to meet B.C. government security standards."
                 />
               </Suspense>
--- frontend/src/pages/Package.tsx
+++ frontend/src/pages/Package.tsx
@@ -1,20 +1,54 @@
-import { PageLayout, Typography } from 'components/common';
+import { Button } from '@bcgov/design-system-react-components';
+import { Heading, PageLayout, Stack, Typography } from 'components/common';
 import { GitHubIssues } from 'components/github';
 
 type PackageProps = {
   repo: string;
+  title: string;
   summary: string;
 };
 
 const Package = (props: PackageProps) => {
-  const { repo, summary } = props;
+  const { repo, title, summary } = props;
+
+  // On press events.
+  const openGitHubRepoInNewTab = () => window.open(`https://github.com/bcgov/${repo}`, '_blank');
+  const openNPMInNewTab = () =>
+    window.open(`https://www.npmjs.com/package/@bcgov/${repo}`, '_blank');
+  const openDocsInNewTab = () => window.open(`https://github.com/bcgov/${repo}/wiki`, '_blank');
 
   return (
     <PageLayout>
       <br />
+      <Heading overline size="small" bold>
+        {title}
+      </Heading>
       <Typography>{summary}</Typography>
       <br />
       <hr />
+      <Stack gap="30px">
+        {/* GITHUB LINK */}
+        <Stack gap="0" additionalStyles={{ alignItems: 'center' }}>
+          <img src="src/assets/github-icon.png" alt="GitHub" width="25px" height="25px" />
+          <Button variant="link" onPress={() => openGitHubRepoInNewTab()}>
+            GitHub Repository
+          </Button>
+        </Stack>
+        {/* NPM LINK */}
+        <Stack gap="0" additionalStyles={{ alignItems: 'center' }}>
+          <img src="src/assets/npm-icon.webp" alt="GitHub" width="35px" height="35px" />
+          <Button variant="link" onPress={() => openNPMInNewTab()}>
+            Package on NPM
+          </Button>
+        </Stack>
+        {/* DOCUMENTATION LINK */}
+        <Stack gap="0" additionalStyles={{ alignItems: 'center' }}>
+          <img src="src/assets/exchange-lab-icon.svg" alt="GitHub" width="25px" height="25px" />
+          <Button variant="link" onPress={() => openDocsInNewTab()}>
+            Package Documentation
+          </Button>
+        </Stack>
+      </Stack>
       <br />
       <GitHubIssues repo={repo} />
     </PageLayout>
