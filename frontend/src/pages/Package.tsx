3c2ae8b68129c1b65f93e70eee7a470c42f8a2e3
BradyMitch <braden.jr.mitch@gmail.com>
2024-04-03 10:03:58 -0700

    Added summary and icon assets

--- frontend/src/pages/Package.tsx
+++ frontend/src/pages/Package.tsx
@@ -1,15 +1,21 @@
-import { PageLayout } from 'components/common';
+import { PageLayout, Typography } from 'components/common';
 import { GitHubIssues } from 'components/github';
 
 type PackageProps = {
   repo: string;
+  summary: string;
 };
 
 const Package = (props: PackageProps) => {
-  const { repo } = props;
+  const { repo, summary } = props;
 
   return (
     <PageLayout>
+      <br />
+      <Typography>{summary}</Typography>
+      <br />
+      <hr />
+      <br />
       <GitHubIssues repo={repo} />
     </PageLayout>
   );
