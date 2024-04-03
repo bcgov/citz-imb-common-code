ed6d676796fdef03526fc95689abd89934b8f754
Scott Toews <scott.toews@gov.bc.ca>
2024-04-03 08:57:13 -0700

    update testing configurations

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,34 +1,19 @@
 import type { Config } from 'jest';
+import { defaults } from 'jest-config';
 
 const config: Config = {
-  verbose: true,
-  roots: ['<rootDir>/src', '<rootDir>/__tests__'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  // setupFilesAfterEnv: ['<rootDir>/__tests__/setupTests.ts'],
+  ...defaults,
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'mjs', 'cjs', 'jsx', 'mts', 'cts', 'json', 'node'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-  // collectCoverage: true,
-  // collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
-  // coverageDirectory: 'coverage',
-  // coverageReporters: ['json', 'lcov', 'text', 'clover'],
-  // coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
-  // snapshotSerializers: ['enzyme-to-json/serializer'],
-  // testEnvironment: 'jsdom',
-  // testURL: 'http://localhost',
-  // testPathIgnorePatterns: ['/node_modules/', '/tests/'],
-  // globals: {
-  //   'ts-jest': {
-  //     tsconfig: 'tsconfig.json',
-  //     diagnostics: {
-  //       ignoreCodes: [151001],
-  //     },
-  //   },
-  // },
+  modulePaths: ['<rootDir>/src/'],
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  verbose: true,
 };
 
 export default config;
