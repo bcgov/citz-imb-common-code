a6b5846f663865d45b55948e24cb2c605639bc5b
Scott Toews <scott.toews@gov.bc.ca>
2024-04-03 11:47:48 -0700

    basic crumb functionality

--- frontend/__tests__/unit/Breadcrumbs/Breadcrumbs.test.tsx
+++ frontend/__tests__/unit/Breadcrumbs/Breadcrumbs.test.tsx
@@ -12,4 +12,17 @@ describe('Breadcrumbs', () => {
       expect(crumbLinks[0].getAttribute('href')).toBe('/');
     });
   });
+
+  describe('when pathname is "/page"', () => {
+    it('should render a two links to home & page', () => {
+      const element = render(<Breadcrumbs pathname="/page" />);
+      const crumbLinks = element.container.querySelectorAll('.crumb > .link');
+
+      expect(crumbLinks.length).toBe(2);
+      expect(crumbLinks[0].childNodes[0].textContent).toBe('Home');
+      expect(crumbLinks[0].getAttribute('href')).toBe('/');
+      expect(crumbLinks[1].childNodes[0].textContent).toBe('Page');
+      expect(crumbLinks[1].getAttribute('href')).toBe('/page');
+    });
+  });
 });
--- frontend/src/components/Breadcrumbs/Breadcrumbs.tsx
+++ frontend/src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,23 +1,47 @@
 import { BreadCrumbProps } from './types';
 
-export const Breadcrumbs = (props: BreadCrumbProps) => {
-  console.log('Breadcrumb props:', props);
+type crumb = {
+  label: string;
+  path: string;
+};
 
-  const pathname = { ...props };
+const makeCrumb = (crumb: string, index: number, myPath: string): crumb | undefined => {
+  if (index === 0 && crumb === '') {
+    return {
+      label: 'Home',
+      path: '/',
+    };
+  }
 
-  console.log(pathname);
+  if (index !== 0 && crumb === '') {
+    return undefined;
+  }
 
-  // const crumbs = pathname.split('/').filter((crumb: string) => crumb !== '');
+  return {
+    label: crumb,
+    path: myPath.slice(0, myPath.indexOf(crumb) + crumb.length),
+  };
+};
 
-  // console.log(crumbs);
+export const Breadcrumbs = ({ pathname }: BreadCrumbProps) => {
+  const myPath = '/page/next/olf/lkj/';
+
+  const crumbs: crumb[] = myPath
+    .split('/')
+    .map((crumb, index) => makeCrumb(crumb, index, myPath))
+    .filter((crumb) => crumb !== undefined) as crumb[];
+
+  console.log('crumbs', crumbs);
 
   return (
     <ul className="breadcrumbs">
-      <li className="crumb">
-        <a className="link" href="/">
-          Home
-        </a>
-      </li>
+      {crumbs.map((crumb, index) => (
+        <li key={index} className="crumb">
+          <a className="link" href={crumb.path}>
+            {crumb.label}
+          </a>
+        </li>
+      ))}
     </ul>
   );
 };
