c7a8bdfda3afc16f352efb055f9f0dfec91b6c65
Scott Toews <scott.toews@gov.bc.ca>
2024-04-09 09:49:53 -0700

    refactor routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,21 @@
 import { useSSO } from '@bcgov/citz-imb-sso-react';
-import { Button, Header } from '@bcgov/design-system-react-components';
-import { PageLayout } from 'components';
-import { useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { ENDPOINTS } from './utils';
-import Router from './Router';
+import { Header } from '@bcgov/design-system-react-components';
+import { AuthButtons, PageLayout } from 'components';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
 const AppRouter = () => {
   const { isAuthenticated, login, logout } = useSSO();
 
-  // Load config when origin changes.
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(ENDPOINTS.CONFIG);
-      const configuration = await response.json();
-      (window as Window).configuration = configuration;
-    })();
-  }, [window.location.origin]);
+  const loginHandler = () => login({ idpHint: 'idir' });
 
   return (
     <>
       <Header title="CITZ IMB Common Code">
-        {isAuthenticated ? (
-          <Button variant="secondary" onPress={() => logout()}>
-            LOGOUT
-          </Button>
-        ) : (
-          <Button
-            variant="secondary"
-            onPress={() => login({ idpHint: 'idir', redirectURL: '/sso-react' })}
-          >
-            LOGIN WITH IDIR
-          </Button>
-        )}
+        <AuthButtons isAuthenticated={isAuthenticated} login={loginHandler} logout={logout} />
       </Header>
       <PageLayout>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </PageLayout>
     </>
   );
--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,58 +1,17 @@
-import { Breadcrumbs, Spinner } from 'components';
-import { Suspense, lazy } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
+import packages from './packages.json';
+import Landing from 'pages/Landing';
+import Package from 'pages/Package';
 
-// Lazy loaded pages.
-const Pages = {
-  Landing: lazy(() => import('pages/Landing')),
-  Package: lazy(() => import('pages/Package')),
-};
+const packageRoutes = packages.map((pkg) => ({
+  path: pkg.pageRoute,
+  element: <Package repo={pkg.repo} title={pkg.title} summary={pkg.details} />,
+}));
 
-const Router = () => {
-  const pathname = useLocation().pathname;
-
-  return (
-    <>
-      <Breadcrumbs pathname={pathname} />
-      <Routes>
-        {/* LANDING PAGE */}
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<Spinner />}>
-              <Pages.Landing />
-            </Suspense>
-          }
-        />
-        {/* SSO React Package */}
-        <Route
-          path="/sso-react"
-          element={
-            <Suspense fallback={<Spinner />}>
-              <Pages.Package
-                repo="citz-imb-sso-react"
-                title="SSO React"
-                summary="This npm package offers an integration solution for React applications requiring authentication through the B.C. government's Single Sign-On SSO (CSS) service. It abstracts the complexity of handling SSO protocols manually. By using this package, developers can quickly implement authentication and authorization in their React applications to meet B.C. government security standards."
-              />
-            </Suspense>
-          }
-        />
-        {/* SSO Express Package */}
-        <Route
-          path="/sso-express"
-          element={
-            <Suspense fallback={<Spinner />}>
-              <Pages.Package
-                repo="citz-imb-sso-express"
-                title="SSO Express"
-                summary="This npm package offers an integration solution for Express applications requiring authentication through the B.C. government's Single Sign-On SSO (CSS) service. It abstracts the complexity of handling SSO protocols manually. By using this package, developers can quickly implement authentication and authorization in their Express applications to meet B.C. government security standards."
-              />
-            </Suspense>
-          }
-        />
-      </Routes>
-    </>
-  );
-};
-
-export default Router;
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />,
+  },
+  ...packageRoutes,
+]);
