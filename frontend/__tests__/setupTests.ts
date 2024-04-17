e77bf86bdafd1b4d096a47bf7f63067096200a77
SDToews <33942967+SDToews@users.noreply.github.com>
2024-04-17 11:47:23 -0700

    Update frontend/__tests__/setupTests.ts
    Co-authored-by: Brady Mitchell <braden.jr.mitch@gmail.com>

--- frontend/__tests__/setupTests.ts
+++ frontend/__tests__/setupTests.ts
@@ -1,5 +1,2 @@
 import '@testing-library/jest-dom';
 
-// jest.mock('@bcgov/citz-imb-sso-react', () => ({
-//   useSSO: () => ({ isAuthenticated: false, login: jest.fn(), logout: jest.fn() }),
-// }));
