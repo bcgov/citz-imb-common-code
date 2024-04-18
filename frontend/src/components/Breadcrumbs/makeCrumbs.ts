9023243ec6de3cdee5d93d73e7be8cc7837c223b
Scott Toews <scott.toews@gov.bc.ca>
2024-04-18 08:26:28 -0700

    removed unneeded file and clarified function purpose

--- frontend/src/components/Breadcrumbs/makeCrumbs.ts
+++ frontend/src/components/Breadcrumbs/makeCrumbs.ts
@@ -1,5 +1,11 @@
 import { CrumbProperty } from './types';
-
+/**
+ * Function that creates an array of objects with the path and label for each path
+ *
+ * @param pathname a string that represents the current path, delimited by '/'
+ * @param labels a string that represents the labels for each path, delimited by '/'
+ * @returns an array of objects with the path and label for each path
+ */
 export const makeCrumbs = (pathname: string, labels: string): CrumbProperty[] => {
   const paths = pathname.split('/');
 
