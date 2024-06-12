e9a5662c2767701764cd0c8d9cc1592703172491
Scott Toews <scott.toews@gov.bc.ca>
2024-06-12 13:41:27 -0700

    added initial data

--- backend/src/database/migrations/1718118307165-initial_setup.ts
+++ backend/src/database/migrations/1718118307165-initial_setup.ts
@@ -7,6 +7,10 @@ export class InitialSetup1718118307165 implements MigrationInterface {
     await queryRunner.query(
       `CREATE TABLE "task" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdOn" TIMESTAMP NOT NULL DEFAULT now(), "lastUpdated" TIMESTAMP NOT NULL DEFAULT now(), "title" text NOT NULL, CONSTRAINT "PK_fb213f79ee45060ba925ecd576e" PRIMARY KEY ("id"))`,
     );
+
+    await queryRunner.query(
+      `INSERT INTO "task" ("id", "createdOn", "lastUpdated", "title") VALUES ('f7b3b3b4-4b1b-4b1b-8b1b-1b1b1b1b1b1b', '2021-10-11 00:00:00', '2021-10-11 00:00:00', 'First Task')`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
